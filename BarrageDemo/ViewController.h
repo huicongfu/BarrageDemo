d299f6e0d15da638de055c4c79f9750d1d2c3077
fu_Mac <fuhuicong@qq.com>
2017-03-11 07:57:47 +0800

    Initial Commit

---
+++ BarrageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BarrageDemo
+//
+//  Created by fu on 17/3/11.
+//  Copyright © 2017年 fhc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BarrageDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BarrageDemo
+//
+//  Created by fu on 17/3/11.
+//  Copyright © 2017年 fhc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
